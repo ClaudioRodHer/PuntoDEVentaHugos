a2724952e667be803e4c9fe986a056d7265f361e
root <isaiasclaudio777@gmail.com>
2017-12-21 14:42:07 -0600

    Merge remote-tracking branch 'refs/remotes/origin/master'
    Conflicts:
    	src/com/Hugos/Controller/PuntoDeVenta_HugosPizza.java

---
+++ src/com/Hugos/Controller/RegistroEntradaController.java
@@ -0,0 +1,15 @@
+
+package com.Hugos.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class RegistroEntradaController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
