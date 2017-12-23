c29ac51c35fb106073aa8d1d9886e8b5cadc5e5a
root <isaiasclaudio777@gmail.com>
2017-12-22 21:28:52 -0600

    Merge remote-tracking branch 'refs/remotes/origin/master'
    Conflicts:
    	src/com/Hugos/Controller/PuntoDeVenta_HugosPizza.java

---
+++ src/com/Hugos/Resources/Notas.java
@@ -0,0 +1,15 @@
+
+package com.Hugos.Resources;
+
+/**
+ *
+ * @author root
+ */
+public class Notas {
+    /*
+    antes de lamar una ventana si ya se declararon sus botones ys e les dio una accion
+    se deben de declarar todas las acciones de los botones de la intefaz
+    pues si no lo hacen se marcara una excepcion y no podran acceder a la ventana
+    
+    */
+}
