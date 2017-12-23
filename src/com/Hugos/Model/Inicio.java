ad2b73fed6df94ac62aa995aada085fa680c6555
root <isaiasclaudio777@gmail.com>
2017-12-22 20:34:36 -0600

    Merge remote-tracking branch 'refs/remotes/origin/master'
    Conflicts:
    	src/com/Hugos/Controller/PuntoDeVenta_HugosPizza.java

--- src/com/Hugos/Model/Inicio.java
+++ src/com/Hugos/Model/Inicio.java
@@ -6,22 +6,26 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+/*
+
+NO LE MUEVAN A ESTA CLASE, PORQUE EL PROYECTOSE TRONARA (O_O)
+*/
+
+///------------- Clase con la cual se llamara el login con el cual se podra accedar a lo demas---------
 
 public class Inicio extends Application {
-    
+   
     @Override
     public void start(Stage primaryStage) throws IOException {
          Parent root=FXMLLoader.load(getClass().getResource("/com/Hugos/View/LoginBien.fxml"));
-        
         Scene scene = new Scene(root);
-        
         primaryStage.setTitle("Login");
         primaryStage.setScene(scene);
         primaryStage.show();
+                
     }
 
-  
-    public static void main(String[] hugos) {
+      public static void main(String[] hugos) {
         launch(hugos);
     }
     
