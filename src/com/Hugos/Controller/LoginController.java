a271da6d4411a81a9a8c1f6202e50108d9d6cf89
root <isaiasclaudio777@gmail.com>
2017-12-22 20:32:08 -0600

    Merge remote-tracking branch 'refs/remotes/origin/master'
    Conflicts:
    	src/com/Hugos/Controller/PuntoDeVenta_HugosPizza.java

--- src/com/Hugos/Controller/LoginController.java
+++ src/com/Hugos/Controller/LoginController.java
@@ -1,27 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.Hugos.Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 
-/**
- * FXML Controller class
- *
- * @author root
- */
 public class LoginController implements Initializable {
+    //variables del textfield y del passwordfiel
+    @FXML
+    private TextField txtUsuario;
+    @FXML
+    private PasswordField passUsuario;
+    //----------------------BOTON ACEPTAR--------------------------------------//
+    //el btnAceptarAcion es el nombre que se le dio al boton enn scenebuilder en code-->On Action:btnAceptarAcion
+    //ahi es donde se hara la accion del boton para poder hacer el evento del boton
+    @FXML
+    private void btnAceptarAcion(ActionEvent evento) throws IOException{
+        if(txtUsuario.getText().equals("Hugo") && passUsuario.getText().equals("1234")){
+            ((Node)(evento.getSource())).getScene().getWindow().hide();//esta linea oculta la ventana anterior
+            //apartir de aqui se hace el llamado a la interfaz del menu
+            Parent menu=FXMLLoader.load(getClass().getResource("/com/Hugos/View/HomeCajero.fxml"));
+            Stage stage=new Stage();
+            Scene scene=new Scene(menu);
+            stage.setScene(scene);
+            stage.setTitle("Menu");
+            stage.show();
+        }else{
+        JOptionPane.showMessageDialog(null,"Usuario Incorrecto");
+        txtUsuario.setText("");
+        passUsuario.setText("");
+        }
+    }
+    //-----------BOTON DE CANCELAR--------------------------------//
+    @FXML
+    private void BtnCancelarAction(ActionEvent evento){
+        JOptionPane.showMessageDialog(null, "Hasta la proxima");
+        System.exit(0);
+    }
+    //-----------BOTON DE REGISTRO DE LA ENTRADA DE LOS EMPLEADOS---------------//
+    @FXML
+    private void BtnRegistroEntradaAction(ActionEvent evento) throws IOException{
+            ((Node)(evento.getSource())).getScene().getWindow().hide();//esta linea oculta la ventana anterior
+            //apartir de aqui se hace el llamado a la interfaz del menu
+            Parent menu=FXMLLoader.load(getClass().getResource("/com/Hugos/View/RegistroEntrada.fxml"));
+            Stage stage=new Stage();
+            Scene scene=new Scene(menu);
+            stage.setScene(scene);
+            stage.setTitle("¿Orden para llevar?");
+            stage.show();
+    }
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
