7222efa0f30e9fe4ebb00dba06ca46c4b25c6d5b
root <isaiasclaudio777@gmail.com>
2017-12-27 16:21:01 -0600

    Merge remote-tracking branch 'refs/remotes/origin/master'
    Conflicts:
    	src/com/Hugos/Controller/PuntoDeVenta_HugosPizza.java

--- src/com/Hugos/Model/Pizzas.java
+++ src/com/Hugos/Model/Pizzas.java
@@ -1,27 +1,8 @@
 
 package com.Hugos.Model;
 
-import java.io.IOException;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-
-public class Pizzas extends Application{
+public class Pizzas {
     private int id_Pizza;
 
 
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-    Parent root=FXMLLoader.load(getClass().getResource("View/HomeCajeroController.fxml"));
-        Scene scene=new Scene(root);
-        primaryStage.setTitle("Hols");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-    public static void main(String [] hugos){
-        launch(hugos);
-    }
 }
