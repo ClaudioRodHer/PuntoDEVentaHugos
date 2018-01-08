5dc0318820ea8f49f395c643e03ffea4401ebd0f
root <isaiasclaudio777@gmail.com>
2018-01-08 01:20:33 -0600

    Merge remote-tracking branch 'refs/remotes/origin/master'
    Conflicts:
    	src/com/Hugos/Controller/PuntoDeVenta_HugosPizza.java

--- src/com/Hugos/Controller/AgregarEmpleadoController.java
+++ src/com/Hugos/Controller/AgregarEmpleadoController.java
@@ -11,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class AgregarEmpleadoController implements Initializable {
@@ -25,22 +26,27 @@ public class AgregarEmpleadoController implements Initializable {
     private TextField txtTelefono;
     @FXML
     private ComboBox cmbPuesto;
-    
+
     @FXML
     private TextField txtContraseña;
-    
+
+    @FXML
+    private TableView tabla;
+
     @FXML
     private void btnAgregarAction() throws ClassNotFoundException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/HugosDB", "root", "");
             Statement estado = (Statement) con.createStatement();
-            estado.executeUpdate("INSERT INTO empleado(nombre,apellido1,apellido2,telefono,puesto,Contraseña) VALUES('" 
+            estado.executeUpdate("INSERT INTO empleado(nombre,apellido1,apellido2,telefono,puesto,Contraseña) VALUES('"
                     + txtNombre.getText() + "',"
                     + "'" + txtApePat.getText() + "'"
-                    + ",'" + txtApeMat.getText() + "','" + txtTelefono.getText() + "','" + cmbPuesto.getSelectionModel().getSelectedItem()+"','"+
-                            txtContraseña.getText()+"')");
+                    + ",'" + txtApeMat.getText() + "','" + txtTelefono.getText() + "','"
+                    + cmbPuesto.getSelectionModel().getSelectedItem() + "','"
+                    + txtContraseña.getText() + "')");
             
+
         } catch (SQLException ex) {
             System.out.println("Error en MySQL: " + ex.getMessage());
         } catch (ClassNotFoundException err) {
@@ -57,7 +63,7 @@ public class AgregarEmpleadoController implements Initializable {
 
     @FXML
     private void btnModificarAction(ActionEvent evento) {
-       
+
     }
 
     @FXML
@@ -65,9 +71,28 @@ public class AgregarEmpleadoController implements Initializable {
 
     }
 
+    @FXML
+    private void tblTablaAction() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/HugosDB", "root", "");
+            Statement tabl = (Statement) con.createStatement();
+            tabl.executeUpdate("SELECT * FROM empleado");
+           tabla.getItems().addAll();
+
+        } catch (SQLException ex) {
+            System.out.println("Error en MySQL: " + ex.getMessage());
+        } catch (ClassNotFoundException err) {
+            err.printStackTrace();
+        } catch (Exception err) {
+            System.out.println("se ha encontrado un error inesperado que es " + err.getMessage());
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        cmbPuesto.getItems().addAll("Administrador","Cajero","Repartidor");
+        cmbPuesto.getItems().addAll("Administrador", "Cajero", "Repartidor");
+       
     }
 
 }
