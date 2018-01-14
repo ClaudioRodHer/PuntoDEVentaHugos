b15613ebd424f6cb294b534677a838bbedb0d440
root <isaiasclaudio777@gmail.com>
2018-01-14 12:34:47 -0600

    Merge remote-tracking branch 'refs/remotes/origin/master'
    Conflicts:
    	src/com/Hugos/Controller/PuntoDeVenta_HugosPizza.java

--- src/com/Hugos/Controller/AgregarEmpleadoController.java
+++ src/com/Hugos/Controller/AgregarEmpleadoController.java
@@ -5,14 +5,17 @@ import com.mysql.jdbc.Driver;
 import com.mysql.jdbc.Statement;
 import java.net.URL;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
 
 public class AgregarEmpleadoController implements Initializable {
 
@@ -46,6 +49,8 @@ public class AgregarEmpleadoController implements Initializable {
                     + cmbPuesto.getSelectionModel().getSelectedItem() + "','"
                     + txtContraseña.getText() + "')");
             
+            mostrar();
+            
 
         } catch (SQLException ex) {
             System.out.println("Error en MySQL: " + ex.getMessage());
@@ -71,14 +76,40 @@ public class AgregarEmpleadoController implements Initializable {
 
     }
 
-    @FXML
-    private void tblTablaAction() {
+   /* @FXML
+    //private void tblTablaAction() {
+    private void mostrar() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/HugosDB", "root", "");
             Statement tabl = (Statement) con.createStatement();
-            tabl.executeUpdate("SELECT * FROM empleado");
-           tabla.getItems().addAll();
+            
+            ResultSet res= tabl.executeQuery("SELECT * FROM empleado;");
+            
+            Object datos[]=new Object[8];
+            //JOptionPane.showConfirmDialog(null,"Hola");
+            while(res.next()){
+                for(int x=0;x<8;x++){
+                    datos[x]=res.getObject(x+1);
+                }
+            }
+            
+            TableColumn col1= new TableColumn("Col1");
+            TableColumn col2= new TableColumn("Col1");
+            TableColumn col3= new TableColumn("Col1");
+            TableColumn col4= new TableColumn("Col1");
+            TableColumn col5= new TableColumn("Col1");
+            TableColumn col6= new TableColumn("Col1");
+            TableColumn col7= new TableColumn("Col1");
+            TableColumn col8= new TableColumn("Col1");
+            
+            tabla.getColumns().addAll(col1,col2,col3,col4,col5,col6,col7,col8);
+            
+// (datos);
+            
+            //tabla.setItems();
+            res.close();
+           //tabla.getItems().addAll();
 
         } catch (SQLException ex) {
             System.out.println("Error en MySQL: " + ex.getMessage());
@@ -87,7 +118,7 @@ public class AgregarEmpleadoController implements Initializable {
         } catch (Exception err) {
             System.out.println("se ha encontrado un error inesperado que es " + err.getMessage());
         }
-    }
+    }*/
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
