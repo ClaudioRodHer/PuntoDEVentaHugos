238d47008aae369badcf26b94961c7781eeb6abb
root <isaiasclaudio777@gmail.com>
2018-01-15 16:56:21 -0600

    Merge remote-tracking branch 'refs/remotes/origin/master'
    Conflicts:
    	src/com/Hugos/Controller/PuntoDeVenta_HugosPizza.java

--- src/com/Hugos/Controller/AgregarEmpleadoController.java
+++ src/com/Hugos/Controller/AgregarEmpleadoController.java
@@ -43,7 +43,7 @@ public class AgregarEmpleadoController implements Initializable {
             Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/HugosDB", "root", "");
             Statement estado = (Statement) con.createStatement();
             estado.executeUpdate("INSERT INTO empleado(nombre,apellido1,apellido2,telefono,puesto,Contraseña) VALUES('"
-                    + txtNombre.getText() + "',"
+                    + txtNombre.    getText() + "',"
                     + "'" + txtApePat.getText() + "'"
                     + ",'" + txtApeMat.getText() + "','" + txtTelefono.getText() + "','"
                     + cmbPuesto.getSelectionModel().getSelectedItem() + "','"
