13724003e2a1acd1efaf5ea822b8f80afb017e7a
root <isaiasclaudio777@gmail.com>
2018-01-15 23:05:09 -0600

    Merge remote-tracking branch 'refs/remotes/origin/master'
    Conflicts:
    	src/com/Hugos/Controller/PuntoDeVenta_HugosPizza.java

--- src/com/Hugos/ConectionDB/ConeccionUsers.java
+++ src/com/Hugos/ConectionDB/ConeccionUsers.java
@@ -1,9 +1,27 @@
 package com.Hugos.ConectionDB;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConeccionUsers {
+
+    Connection conect = null;
+
+    public Connection conexion() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conect = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/HugosDB", "root", "");
+
+            // mostrar();
+        } catch (SQLException ex) {
+            System.out.println("Error en MySQL: " + ex.getMessage());
+        } catch (ClassNotFoundException err) {
+            err.printStackTrace();
+        } catch (Exception err) {
+            System.out.println("se ha encontrado un error inesperado que es " + err.getMessage());
+        }
+        return conect;
+    }
     
-//haciendo ṕull desde netbeans
 }
-    
-    
